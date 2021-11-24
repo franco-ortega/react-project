a9ad9acac39d90f66ec23d1f52963e04a481a22e
Franco Ortega <francopdx@gmail.com>
2021-11-24 11:00:46 -0800

    add React

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      Good morning.
+      <p>Paragraph</p>
+    </div>
+  );
+};
+
+export default App;
